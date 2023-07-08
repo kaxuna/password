d43b21bcc965eee768f9481f9582d80e12651ab2
kakhaphilauri <kakhapilauri@gmail.com>
2023-07-08 21:33:39 +0400

    Updates

---
+++ precache-manifest.de50f5cc05eb729dc9c6c1641f877523.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "609224e9c60110a4484d",
+    "url": "/static/css/main.4ffeb16c.chunk.css"
+  },
+  {
+    "revision": "609224e9c60110a4484d",
+    "url": "/static/js/main.609224e9.chunk.js"
+  },
+  {
+    "revision": "ac24e3b79f3b666f9bf4",
+    "url": "/static/js/1.ac24e3b7.chunk.js"
+  },
+  {
+    "revision": "662c98e2621ba87fae22",
+    "url": "/static/js/2.662c98e2.chunk.js"
+  },
+  {
+    "revision": "1a4323d103066e716c12",
+    "url": "/static/js/runtime~main.1a4323d1.js"
+  },
+  {
+    "revision": "d53e6160a36042c6d552706824077772",
+    "url": "/static/media/img.d53e6160.png"
+  },
+  {
+    "revision": "776b823e12126cb16dfda80306a357ce",
+    "url": "/index.html"
+  }
+];
